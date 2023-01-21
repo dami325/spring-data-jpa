4f272a478aeaf4c810a8af90e1654128bb30c8c5
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-21 17:21:44 +0900

    JPA NamedQuery

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername") /**생략 해도 Member.findByUsername 쿼리 네임을 먼저 찾는다. 이후 없으면 메소드 이름으로 생성 */
+    List<Member> findByUsername(@Param("username") String username);
 }
